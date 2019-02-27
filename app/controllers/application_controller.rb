0d36225d1129a2d5eee3e9afd3d6ff81bb75aae9
AgonIdrizi <agon.88@hotmail.com>
2019-02-27 16:34:08 +0100

    add cart controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   include ApplicationHelper
+  helper_method
+
+  def current_order
+  	if !session[:order_id].nil?
+  	  Order.find(session[:order_id])
+  	else
+  	  Order.new
+  	end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+
   root to: 'items#index'
   devise_for :users, controllers: { registrations: 'users/registrations' }
   resource :cart, only: [:show]
