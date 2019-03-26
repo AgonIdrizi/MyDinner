b0c361d81f8cd82e212b04d18f97e49592c0209b
AgonIdrizi <agon.88@hotmail.com>
2019-03-26 15:50:51 +0100

    models spec for review add

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,7 +1,12 @@
 class Review < ApplicationRecord
   belongs_to :user
   belongs_to :item
-
+  validate :review_can_be_editet_after_15_of_creation , on: :update
   validates :user_id, presence: true, uniqueness: {scope: :item_id}
   
+  def review_can_be_editet_after_15_of_creation
+  	if self.created_at + 15.minutes > Time.now
+  	  errors.add(:base, 'Review cant be edited, 15 min after creation')
+  	end
+  end
 end
--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe Review, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  before do
+    @user = FactoryBot.create(:user)
+    @item = FactoryBot.create(:item)
+  end
+  describe 'Review creation'do
+    it 'creates a review' do
+      @review = FactoryBot.build(:review, user_id: @user.id, item_id: @item.id)
+      @review.save
+      expect(@review.persisted?).to eq(true)
+    end
+  end
+
+  describe 'Review update' do
+  	it 'cant update review,within 15minutes after it was created' do
+  	  @review = FactoryBot.build(:review, user_id: @user.id, item_id: @item.id)
+      @review.save
+
+      @review.update_attributes(star_rating: 5)
+      expect(@review.errors.full_messages).to include('Review cant be edited, 15 min after creation')
+  	end
+  end
 end
