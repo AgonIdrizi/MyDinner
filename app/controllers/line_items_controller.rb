5f847f494de39ac18bee8bca1ce66c7e0c4cec47
AgonIdrizi <agon.88@hotmail.com>
2019-06-13 16:11:21 +0200

    add ajax requests for cart update/delete line_items

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -1,17 +1,16 @@
 class LineItemsController < ApplicationController
+  before_action :set_order, only: [:create, :update, :destroy]
   def create
-  	
-  	@order = current_order
   	@line_item = @order.line_items.find_or_initialize_by(item_id: params[:line_item][:item_id].to_i)
-  	
+    @item = @line_item.item
+    @line_items = @order.line_items
   	@line_item.quantity =  @line_item.quantity == nil ? 
                             params[:line_item][:quantity].to_i : 
                             @line_item.quantity + params[:line_item][:quantity].to_i
-	  	
-
 	respond_to do |format|
 	  if @line_item.save
-	  	format.html { redirect_to root_path, notice: 'Successfully added to cart.'}
+      format.html { redirect_to root_path, notice: 'Successfully added to cart.'}
+      format.js
 	  else
 	  	format.html { render action: 'new' }
 	  end
@@ -20,10 +19,9 @@ class LineItemsController < ApplicationController
   end
 
   def update
-  	@order = current_order
-
   	@line_item = @order.line_items.find_by(item_id: params[:line_item][:item_id])
     @item = @line_item.item
+    @line_items = @order.line_items
   	respond_to do |format|
       if @line_item.update(line_item_params)
         format.html { redirect_to cart_path, notice: 'Successfully updated the order item.' }
@@ -33,12 +31,11 @@ class LineItemsController < ApplicationController
       end
     end
 
-  	#@line_item.update_attributes(line_item_params)
-  	@line_item = @order.line_items
+  	
+  
   end
 
   def destroy
-  	@order = current_order
   	@line_item = @order.line_items.find(params[:id])
     @line_item.destroy
     respond_to do |format|
@@ -51,5 +48,9 @@ class LineItemsController < ApplicationController
 private
   def line_item_params
   	params.require(:line_item).permit(:quantity, :item_id)
+  end
+
+  def set_order
+    @order = current_order
   end
 end
